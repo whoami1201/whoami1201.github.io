33341865423dbc4a56292820e7a42aec38f8337b
Bao Nguyen <baonguyen1201@gmail.com>
2014-04-06 20:23:19 +0300

    Add punch of stuffs, move to a subfolder

--- us/js/script.js
+++ us/js/script.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
+
 	$("#content-wrapper > div").each(function(e){
 		if (e!=0) {
 			$(this).hide();
 		}
-	})
-	$("#up-nav").click(function(){
+    })
+
+	$("#a-up-nav").click(function(){
 		if ($("#content-wrapper > div:visible").prev().length != 0)
             $("#content-wrapper > div:visible").fadeOut(function(){
                 $(this).prev().fadeIn();
             });
         return false;
     })
-	$("#down-nav").click(function(){
-		if ($("#content-wrapper > div:visible").next().length != 0)
+
+	$("#a-down-nav").click(function(){
+		if ($("#content-wrapper > div:visible").next().length != 0) 
             $("#content-wrapper > div:visible").fadeOut(function(){
                 $(this).next().fadeIn();
             });
